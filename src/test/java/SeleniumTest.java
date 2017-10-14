fbbb19a70ef063e35312f70a125ebb9aa21bcda7
Robin Van den Broeck <robin.broeck@gmail.com>
2017-10-14 14:52:10 +0200

    Added WebdriverManager and started using ChromeDriver

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,12 +1,21 @@
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.FirefoxDriverManager;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public abstract class SeleniumTest {
-    protected final WebDriver driver;
+    protected WebDriver driver;
 
+    @BeforeClass
+    public static void setupClass() {
+        ChromeDriverManager.getInstance().setup();
+    }
 
-    protected SeleniumTest() {
-        System.setProperty("webdriver.gecko.driver", "C:\\Webdriver\\geckodriver.exe");
-        driver = new FirefoxDriver();
+    @Before
+    public void setupTest() {
+        driver = new ChromeDriver();
     }
 }

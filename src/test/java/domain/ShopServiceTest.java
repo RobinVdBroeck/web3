36b5d46a92955dc705d4e0098d47d112022db464
Robin Van den Broeck <robin.broeck@gmail.com>
2017-12-29 14:34:24 +0100

    Simplified ShopServiceTest

--- src/test/java/domain/ShopServiceTest.java
+++ src/test/java/domain/ShopServiceTest.java
@@ -9,28 +9,21 @@ import org.junit.Test;
 public class ShopServiceTest {
     private ShopService service;
     private Person person;
-    private String userid;
     private final String password = "1234";
 
     @Before
     public void setUp() {
         service = new ShopService();
-        userid = generateRandomUseridInOrderToRunTestMoreThanOnce("sinterklaas");
-        person = new Person(userid, "klaas@klaas.be", password, "Klaas", "Claesens");
+        person = new Person("sinterklaas", "klaas@klaas.be", password, "Klaas", "Claesens");
     }
 
     @Test
     public void getPerson_should_return_the_person_if_registered() {
         service.addPerson(person);
 
-        Person personRetrieved = service.getPerson(userid);
+        Person personRetrieved = service.getPerson(person.getUserid());
 
-        assertNotNull(personRetrieved);
-        assertTrue(personRetrieved.isCorrectPassword(password));
-        assertEquals(userid, personRetrieved.getUserid());
-        assertEquals(person.getEmail(), personRetrieved.getEmail());
-        assertEquals(person.getFirstName(), personRetrieved.getFirstName());
-        assertEquals(person.getLastName(), personRetrieved.getLastName());
+        assertEquals(person, personRetrieved);
     }
 
     @Test
@@ -46,11 +39,9 @@ public class ShopServiceTest {
     public void getUserIfAuthenticated_should_return_the_person_if_registered_and_correct_password() {
         service.addPerson(person);
 
-        Person personRetrieved = service.getUserIfAuthenticated(userid, password);
+        Person personRetrieved = service.getUserIfAuthenticated(person.getUserid(), password);
 
-        assertNotNull(personRetrieved);
-        assertTrue(personRetrieved.isCorrectPassword(password));
-        assertEquals(userid, personRetrieved.getUserid());
+        assertEquals(person, personRetrieved);
     }
 
     @Test
@@ -66,14 +57,8 @@ public class ShopServiceTest {
     public void getUserIfAuthenticated_should_return_null_if_person_is_registered_but_incorrect_password() {
         service.addPerson(person);
 
-        Person personRetrieved = service.getUserIfAuthenticated(userid, "WrongPassword");
+        Person personRetrieved = service.getUserIfAuthenticated(person.getUserid(), "WrongPassword");
 
         assertNull(personRetrieved);
     }
-
-    private String generateRandomUseridInOrderToRunTestMoreThanOnce(String component) {
-        int random = (int) (Math.random() * 1000 + 1);
-        return random + component;
-    }
-
 }
